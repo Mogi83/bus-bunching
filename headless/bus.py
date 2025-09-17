09c64fc6d45cc6d2cd7f84017ca251c613538d79
Evan <evancb45@gmail.com>
2025-09-17 08:03:07 -0600

    Implemented a simple bus loop

--- headless/bus.py
+++ headless/bus.py
@@ -1,8 +1,31 @@
-class Bus:
-    def __init__(self, bus_id,  env):
-        self.env = env
-        self.action = env.process(self.run())
+#
+# Handles the entirety of the bus logic
+#
+import simpy
+import random
 
-    def run(self):
+class Bus:
+    def __init__(self, env):
+        self.env = env
+        self.board_proc = self.env.process(self.travel())
+        self.loading_proc = self.env.process(self.load())
+        self.loading_completed = self.env.event()
+        self.bus_stopping = self.env.event()
+
+    def travel(self):
         while True:
-            pass
+            print("Leaving stop at time %d" % self.env.now)
+            yield self.env.timeout(random.randint(5,11))
+            print("Arriving at a stop at time %d" % self.env.now)
+            self.bus_stopping.succeed()
+            self.bus_stopping = self.env.event()
+            yield self.loading_completed
+
+    def load(self):
+        while True:
+            yield self.bus_stopping
+            print("Passengers hopping on at time %d" % self.env.now)
+            yield self.env.timeout(random.randint(2,5))
+            print("Passengers loaded at time %d" % self.env.now)
+            self.loading_completed.succeed()
+            self.loading_completed = self.env.event()
--- headless/simulate.py
+++ headless/simulate.py
@@ -1,3 +1,11 @@
+#
+# Designed to serve as the overall
+# controller of the simulation
+#
 import simpy
-import random
+from bus import Bus
 
+
+env = simpy.Environment()
+bus = Bus(env)
+env.run(until=30)
