73c5b953b506a5ad09214f42df967e6710faf8fe
Evan <evan.bullwinkle@gmail.com>
2025-09-25 10:39:25 -0600

    Refactor Bus of bus class to have more specific identifiers. Additionally updated simulation to create multiple buses and log thier events.

--- headless/bus.py
+++ headless/bus.py
@@ -5,27 +5,61 @@ import simpy
 import random
 
 class Bus:
-    def __init__(self, env):
+    def __init__(self, env, bus_id, stops, collector, interval):
+        #var assignment
         self.env = env
+        self.id = bus_id
+        self.stops = stops
+        self.collector = collector
+        self.interval = interval
+        self.last_arrival_time = None
+        self.current_stop = None
+
+        #processes
         self.board_proc = self.env.process(self.travel())
         self.loading_proc = self.env.process(self.load())
         self.loading_completed = self.env.event()
         self.bus_stopping = self.env.event()
 
+
     def travel(self):
+        stop_idx = 0
         while True:
-            print("Leaving stop at time %d" % self.env.now)
-            yield self.env.timeout(random.randint(5,11))
-            print("Arriving at a stop at time %d" % self.env.now)
+            if self.last_arrival_time == None:
+                arrival_time = self.env.now
+            else:
+                arrival_time = self.last_arrival_time + self.interval + random.randint(5,11) #Magic number related to delay
+            
+            self.current_stop = self.stops[stop_idx]
+            self.collector.append({
+                "bus_id": self.id,
+                "event": "arriving",
+                "stop": self.current_stop,
+                "time": arrival_time
+            })
+
+            self.last_arrival_time = arrival_time
+            stop_idx = (stop_idx + 1 ) % len(self.stops)
+
+            yield self.env.timeout(random.randint(5,11)) #Magic number related to dwell time.
+
             self.bus_stopping.succeed()
             self.bus_stopping = self.env.event()
+
             yield self.loading_completed
 
     def load(self):
         while True:
             yield self.bus_stopping
-            print("Passengers hopping on at time %d" % self.env.now)
-            yield self.env.timeout(random.randint(2,5))
-            print("Passengers loaded at time %d" % self.env.now)
+
+            yield self.env.timeout(random.randint(2,5)) #Magic number for loading time
+
+            self.collector.append({
+                "bus_id": self.id,
+                "event": "departing",
+                "stop": self.current_stop,
+                "time": self.env.now
+            })
+            
             self.loading_completed.succeed()
             self.loading_completed = self.env.event()
--- headless/simulate.py
+++ headless/simulate.py
@@ -3,9 +3,15 @@
 # controller of the simulation
 #
 import simpy
+import pandas as pd
 from bus import Bus
 
 
+event_log = []
 env = simpy.Environment()
-bus = Bus(env)
+buses = [Bus(env, bus_id=i, stops=["Stop 1", "Stop 2", "Stop 3", "Stop 4"], collector=event_log, interval=8) for i in range(3)]
+
 env.run(until=30)
+
+df = pd.DataFrame(event_log)
+print(df)
